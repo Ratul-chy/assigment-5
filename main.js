ff7d42be65cf79c69d1d0ea756a023fd7826d86e
ratul-chy <mdratuldesign@gmail.com>
2026-03-07 21:02:00 +0600

    3rd commit

--- main.js
+++ main.js
@@ -14,3 +14,28 @@ alert("Invalid Credentials")
 }
 
 }
+
+const loadissues =  () => {
+    fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    .then((res) => res.json())
+    .then((json) => displayIssues(json.data))
+}
+
+const displayIssues = (issues) => {
+    const container = document.getElementById("issuesContainer");
+    container.innerHTML = "";
+    for(let issue of issues){
+        const card = document.createElement("div");
+        card.innerHTML = `
+        <h3>${issue.title}</h3>
+        <p>${issue.description}</p>
+        <p>Status: ${issue.status}</p>
+        `
+        card.classList.add("card");
+        card.style.borderTop = issue.status === "open" ? "5px solid green" : "5px solid red";
+        container.appendChild(card);
+    }
+}
+
+loadissues()
+
